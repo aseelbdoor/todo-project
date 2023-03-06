337adb5472805ed64467af217990eabfb00a8b02
aseelbdoor <aseel.bdoor2001@gmail.com>
2023-03-06 08:36:50 +0000

    new thing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+// lab 5
 var userName;
 var gender;
 var age;
@@ -10,10 +11,8 @@ while (age<=0){
     age=prompt("Your age shoudn't be less than or equal 0");
 }
 welcom=confirm("Did you need to skip welcom message? ");
-console.log(welcom);
 
 if(!welcom){
-    console.log(gender);
     if(gender.toLocaleUpperCase() == "MALE"){
         alert("Mr "+userName+" welcome in our website" );
     }
@@ -24,3 +23,38 @@ if(!welcom){
         alert("Welcome in our website" );
     }
 }
+// lab 6
+function checkAnswers(specificAnswer,repeateQuestions){
+    specificAnswer=specificAnswer.toLocaleLowerCase();
+    specificAnswer=specificAnswer.trim();
+    if(specificAnswer===null || specificAnswer==""){
+        allAnswers.push("invalid answer");
+    }
+    else if (specificAnswer=="yes" || specificAnswer=="no"){
+        allAnswers.push(specificAnswer);
+    }
+    else{
+        let validAnswers=prompt("Please just type yes or no to answer the questions\n"+repeateQuestions);
+        checkAnswers(validAnswers,repeateQuestions);
+    }
+}
+
+function askQuestions(que){
+    for(let i=0;i<que.length;i++){
+        let answer= prompt("Please answer with yes or no only \n "+que[i]);
+        checkAnswers(answer,que[i])
+    }
+}
+
+function questionsWithAnswers(allQuestions,allAnswers){
+    for(let i=0;i<allQuestions.length;i++){
+        console.log(allAnswers[i]);
+    }
+}
+
+// Main code
+const allQuestions= ["Do you have previous experience or projects in web development?",
+"Do you prefer Telecommuting?","Do you have a laptop with high specifications?"];
+const allAnswers=[];
+askQuestions(allQuestions);
+questionsWithAnswers(allQuestions,allAnswers);
